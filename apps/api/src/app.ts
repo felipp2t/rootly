c6453e6bc2b9c3b6a03865d1635d0f7ab323c4fc
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-03 15:56:09 -0300

    feat: use fastify-type-provider-zod and Zod compilers

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,15 @@
 import cors from '@fastify/cors'
 import { fastify } from 'fastify'
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 await app.register(cors, {
   origin: 'http://localhost:3000',
 })
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.get('/health', async () => {
   return { status: 'ok' }
