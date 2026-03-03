f4e4b2aeacb2975e5dd6b8a9626b3adf93cf2987
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-03 13:17:49 -0300

    test: add in-memory test repos for folder and item

---
+++ apps/api/test/repositories/folder-repository-in-memory.ts
@@ -0,0 +1,31 @@
+import type { FolderRepository } from '@/domain/root/application/repositories/folder-repository.ts'
+import type { Folder } from '@/domain/root/enterprise/entities/folder.ts'
+
+export class FolderRepositoryInMemory implements FolderRepository {
+  folders: Folder[] = []
+
+  async findById(id: string): Promise<Folder | null> {
+    return this.folders.find((folder) => folder.id.toString() === id) ?? null
+  }
+
+  async findByName(name: string): Promise<Folder | null> {
+    return this.folders.find((folder) => folder.name === name) ?? null
+  }
+
+  async findAll(): Promise<Folder[]> {
+    return this.folders
+  }
+
+  async save(folder: Folder): Promise<void> {
+    this.folders.push(folder)
+  }
+  async delete(id: string): Promise<void> {
+    const folderIndex = this.folders.findIndex(
+      (folder) => folder.id.toString() === id,
+    )
+
+    if (folderIndex !== -1) {
+      this.folders.splice(folderIndex, 1)
+    }
+  }
+}
---
+++ apps/api/test/repositories/item-repository-in-memory.ts
@@ -0,0 +1,30 @@
+import type { ItemRepository } from '@/domain/root/application/repositories/item-repository.ts'
+import type { Item } from '@/domain/root/enterprise/entities/item.ts'
+
+export class ItemRepositoryInMemory implements ItemRepository {
+  items: Item[] = []
+
+  async findById(id: string): Promise<Item | null> {
+    return this.items.find((item) => item.id.toString() === id) ?? null
+  }
+
+  async findByTitle(title: string): Promise<Item | null> {
+    return this.items.find((item) => item.title === title) ?? null
+  }
+
+  async findAll(): Promise<Item[]> {
+    return this.items
+  }
+
+  async save(item: Item): Promise<void> {
+    this.items.push(item)
+  }
+
+  async delete(id: string): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id.toString() === id)
+
+    if (itemIndex !== -1) {
+      this.items.splice(itemIndex, 1)
+    }
+  }
+}
