09cfd51846e0f38e521deb3241f00fb32a0c2613
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-04 10:25:22 -0300

    fix: make InvalidItemTypeError implement BaseError

--- apps/api/src/domain/root/enterprise/validators/_errors/invalid-item-type.ts
+++ apps/api/src/domain/root/enterprise/validators/_errors/invalid-item-type.ts
@@ -1,4 +1,6 @@
-export class InvalidItemTypeError extends Error {
+import type { BaseError } from '@/core/errors/base-error.ts'
+
+export class InvalidItemTypeError extends Error implements BaseError {
   constructor(message?: string) {
     super(message ?? 'Invalid item type')
   }
