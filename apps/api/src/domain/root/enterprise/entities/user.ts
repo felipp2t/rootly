5b95d7dafd6cddc8f29899157ce032b4e53043fe
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-04 17:47:01 -0300

    feat: add User entity

---
+++ apps/api/src/domain/root/enterprise/entities/user.ts
@@ -0,0 +1,47 @@
+import { Entity } from '@/core/entities/entity.ts'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id.ts'
+import type { Optional } from '@/core/types/optional.ts'
+
+interface UserProps {
+  email: string
+  name: string
+  passwordHash: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+export class User extends Entity<UserProps> {
+  get email() {
+    return this.props.email
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  get passwordHash() {
+    return this.props.passwordHash
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  static create(
+    props: Optional<UserProps, 'createdAt' | 'updatedAt'>,
+    id?: UniqueEntityID,
+  ) {
+    return new User(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+        updatedAt: props.updatedAt ?? new Date(),
+      },
+      id ?? new UniqueEntityID(crypto.randomUUID()),
+    )
+  }
+}
