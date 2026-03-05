dac20d52e79d3eea76c2f767961da9979f4903fe
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-05 10:19:34 -0300

    feat: add Argon2-based hasher and dependency

---
+++ apps/api/src/infra/cryptography/argon-hasher.ts
@@ -0,0 +1,19 @@
+import { hash, verify } from 'argon2'
+import type { HashComparer } from '@/domain/root/application/cryptography/hash-comparer.ts'
+import type { HashGenerator } from '@/domain/root/application/cryptography/hash-generator.ts'
+
+export class BcryptHasher implements HashGenerator, HashComparer {
+  private HASH_SALT_LENGTH = 8
+
+  hash(plain: string): Promise<string> {
+    return hash(plain, {
+      salt: Buffer.from(
+        crypto.getRandomValues(new Uint8Array(this.HASH_SALT_LENGTH)),
+      ),
+    })
+  }
+
+  compare(plain: string, hash: string): Promise<boolean> {
+    return verify(plain, hash)
+  }
+}
