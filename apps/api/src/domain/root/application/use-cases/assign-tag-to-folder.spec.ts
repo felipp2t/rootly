121dfb4d3d29887d27b73384ad7ff77ef9c211f0
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-06 15:45:40 -0300

    feat: add tests for assigning tags to folder and item

---
+++ apps/api/src/domain/root/application/use-cases/assign-tag-to-folder.spec.ts
@@ -0,0 +1,118 @@
+import { makeFolder } from '@test/factories/make-folder.ts'
+import { makeTag } from '@test/factories/make-tag.ts'
+import { makeUser } from '@test/factories/make-user.ts'
+import { makeWorkspace } from '@test/factories/make-workspace.ts'
+import { InMemoryFolderRepository } from '@test/repositories/in-memory-folder-repository.ts'
+import { InMemoryTagRepository } from '@test/repositories/in-memory-tag-repository.ts'
+import { FolderNotFoundError } from './_errors/folder-not-found-error.ts'
+import { TagNotFoundError } from './_errors/tag-not-found-error.ts'
+import { AssignTagToFolderUseCase } from './assign-tag-to-folder.ts'
+
+let folderRepository: InMemoryFolderRepository
+let tagRepository: InMemoryTagRepository
+let sut: AssignTagToFolderUseCase
+
+describe('AssignTagToFolder', () => {
+  beforeEach(() => {
+    folderRepository = new InMemoryFolderRepository()
+    tagRepository = new InMemoryTagRepository()
+    sut = new AssignTagToFolderUseCase(folderRepository, tagRepository)
+  })
+
+  it('should be able to assign a tag to a folder', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const folder = makeFolder({ workspaceId: workspace.id.toString() })
+    const tag = makeTag({ workspaceId: workspace.id.toString() })
+
+    await folderRepository.save(folder)
+    await tagRepository.save(tag)
+
+    const response = await sut.execute({
+      folderId: folder.id.toString(),
+      tagId: tag.id.toString(),
+    })
+
+    expect(response.isRight()).toBe(true)
+    expect(folderRepository.folders[0].tagIds).toContain(tag.id.toString())
+  })
+
+  it('should be able to assign multiple tags to a folder', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const folder = makeFolder({ workspaceId: workspace.id.toString() })
+    const tagA = makeTag({ workspaceId: workspace.id.toString() })
+    const tagB = makeTag({ workspaceId: workspace.id.toString() })
+
+    await folderRepository.save(folder)
+    await tagRepository.save(tagA)
+    await tagRepository.save(tagB)
+
+    await sut.execute({
+      folderId: folder.id.toString(),
+      tagId: tagA.id.toString(),
+    })
+    await sut.execute({
+      folderId: folder.id.toString(),
+      tagId: tagB.id.toString(),
+    })
+
+    expect(folderRepository.folders[0].tagIds).toHaveLength(2)
+    expect(folderRepository.folders[0].tagIds).toContain(tagA.id.toString())
+    expect(folderRepository.folders[0].tagIds).toContain(tagB.id.toString())
+  })
+
+  it('should be idempotent when assigning the same tag twice', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const folder = makeFolder({ workspaceId: workspace.id.toString() })
+    const tag = makeTag({ workspaceId: workspace.id.toString() })
+
+    await folderRepository.save(folder)
+    await tagRepository.save(tag)
+
+    await sut.execute({
+      folderId: folder.id.toString(),
+      tagId: tag.id.toString(),
+    })
+    const response = await sut.execute({
+      folderId: folder.id.toString(),
+      tagId: tag.id.toString(),
+    })
+
+    expect(response.isRight()).toBe(true)
+    expect(folderRepository.folders[0].tagIds).toHaveLength(1)
+  })
+
+  it('should not be able to assign a tag to a non-existing folder', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const tag = makeTag({ workspaceId: workspace.id.toString() })
+
+    await tagRepository.save(tag)
+
+    const response = await sut.execute({
+      folderId: 'non-existing-folder-id',
+      tagId: tag.id.toString(),
+    })
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toBeInstanceOf(FolderNotFoundError)
+  })
+
+  it('should not be able to assign a non-existing tag to a folder', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const folder = makeFolder({ workspaceId: workspace.id.toString() })
+
+    await folderRepository.save(folder)
+
+    const response = await sut.execute({
+      folderId: folder.id.toString(),
+      tagId: 'non-existing-tag-id',
+    })
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toBeInstanceOf(TagNotFoundError)
+  })
+})
---
+++ apps/api/src/domain/root/application/use-cases/assign-tag-to-item.spec.ts
@@ -0,0 +1,114 @@
+import { makeFolder } from '@test/factories/make-folder.ts'
+import { makeItem } from '@test/factories/make-item.ts'
+import { makeTag } from '@test/factories/make-tag.ts'
+import { makeUser } from '@test/factories/make-user.ts'
+import { makeWorkspace } from '@test/factories/make-workspace.ts'
+import { InMemoryItemRepository } from '@test/repositories/in-memory-item-repository.ts'
+import { InMemoryTagRepository } from '@test/repositories/in-memory-tag-repository.ts'
+import { ItemNotFoundError } from './_errors/item-not-found-error.ts'
+import { TagNotFoundError } from './_errors/tag-not-found-error.ts'
+import { AssignTagToItemUseCase } from './assign-tag-to-item.ts'
+
+let itemRepository: InMemoryItemRepository
+let tagRepository: InMemoryTagRepository
+let sut: AssignTagToItemUseCase
+
+describe('AssignTagToItem', () => {
+  beforeEach(() => {
+    itemRepository = new InMemoryItemRepository()
+    tagRepository = new InMemoryTagRepository()
+    sut = new AssignTagToItemUseCase(itemRepository, tagRepository)
+  })
+
+  it('should be able to assign a tag to an item', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const folder = makeFolder({ workspaceId: workspace.id.toString() })
+    const item = makeItem({ folderId: folder.id.toString() })
+    const tag = makeTag({ workspaceId: workspace.id.toString() })
+
+    await itemRepository.save(item)
+    await tagRepository.save(tag)
+
+    const response = await sut.execute({
+      itemId: item.id.toString(),
+      tagId: tag.id.toString(),
+    })
+
+    expect(response.isRight()).toBe(true)
+    expect(itemRepository.items[0].tagIds).toContain(tag.id.toString())
+  })
+
+  it('should be able to assign multiple tags to an item', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const folder = makeFolder({ workspaceId: workspace.id.toString() })
+    const item = makeItem({ folderId: folder.id.toString() })
+    const tagA = makeTag({ workspaceId: workspace.id.toString() })
+    const tagB = makeTag({ workspaceId: workspace.id.toString() })
+
+    await itemRepository.save(item)
+    await tagRepository.save(tagA)
+    await tagRepository.save(tagB)
+
+    await sut.execute({ itemId: item.id.toString(), tagId: tagA.id.toString() })
+    await sut.execute({ itemId: item.id.toString(), tagId: tagB.id.toString() })
+
+    expect(itemRepository.items[0].tagIds).toHaveLength(2)
+    expect(itemRepository.items[0].tagIds).toContain(tagA.id.toString())
+    expect(itemRepository.items[0].tagIds).toContain(tagB.id.toString())
+  })
+
+  it('should be idempotent when assigning the same tag twice', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const folder = makeFolder({ workspaceId: workspace.id.toString() })
+    const item = makeItem({ folderId: folder.id.toString() })
+    const tag = makeTag({ workspaceId: workspace.id.toString() })
+
+    await itemRepository.save(item)
+    await tagRepository.save(tag)
+
+    await sut.execute({ itemId: item.id.toString(), tagId: tag.id.toString() })
+    const response = await sut.execute({
+      itemId: item.id.toString(),
+      tagId: tag.id.toString(),
+    })
+
+    expect(response.isRight()).toBe(true)
+    expect(itemRepository.items[0].tagIds).toHaveLength(1)
+  })
+
+  it('should not be able to assign a tag to a non-existing item', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const tag = makeTag({ workspaceId: workspace.id.toString() })
+
+    await tagRepository.save(tag)
+
+    const response = await sut.execute({
+      itemId: 'non-existing-item-id',
+      tagId: tag.id.toString(),
+    })
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toBeInstanceOf(ItemNotFoundError)
+  })
+
+  it('should not be able to assign a non-existing tag to an item', async () => {
+    const user = makeUser()
+    const workspace = makeWorkspace({ userId: user.id.toString() })
+    const folder = makeFolder({ workspaceId: workspace.id.toString() })
+    const item = makeItem({ folderId: folder.id.toString() })
+
+    await itemRepository.save(item)
+
+    const response = await sut.execute({
+      itemId: item.id.toString(),
+      tagId: 'non-existing-tag-id',
+    })
+
+    expect(response.isLeft()).toBe(true)
+    expect(response.value).toBeInstanceOf(TagNotFoundError)
+  })
+})
