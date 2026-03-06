fdc46d80b212d95fab9c3991422d1e4a775787cf
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-06 18:03:43 -0300

    feat: register HTTP controllers in app

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -5,6 +5,10 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+import { authenticateUserController } from './infra/http/controllers/authenticate-user.controller.ts'
+import { createAccountController } from './infra/http/controllers/create-account.controller.ts'
+import { createFolderController } from './infra/http/controllers/create-folder.controller.ts'
+import { createItemsController } from './infra/http/controllers/create-item.controller.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -18,5 +22,10 @@ app.setSerializerCompiler(serializerCompiler)
 app.get('/health', async () => {
   return { status: 'ok' }
 })
+
+app.register(createAccountController)
+app.register(authenticateUserController)
+app.register(createFolderController)
+app.register(createItemsController)
 
 export { app }
