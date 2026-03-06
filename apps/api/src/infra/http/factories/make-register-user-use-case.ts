922e76502bc2cfe0a550c5efa681d4f78de3b0a5
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-06 15:04:03 -0300

    fix: provide workspace repository to register use case

--- apps/api/src/infra/http/factories/make-register-user-use-case.ts
+++ apps/api/src/infra/http/factories/make-register-user-use-case.ts
@@ -2,10 +2,16 @@ import { RegisterUserUseCase } from '@/domain/root/application/use-cases/registe
 import { ArgonHasher } from '@/infra/cryptography/argon-hasher.ts'
 import { db } from '@/infra/db/drizzle/index.ts'
 import { DrizzleUserRepository } from '@/infra/db/drizzle/repositories/user-respository.ts'
+import { DrizzleWorkspaceRepository } from '@/infra/db/drizzle/repositories/workspace-repository.ts'
 
 export function makeRegisterUserUseCase() {
   const userRepository = new DrizzleUserRepository(db)
   const hashRepository = new ArgonHasher()
-  const useCase = new RegisterUserUseCase(userRepository, hashRepository)
+  const workspaceRepository = new DrizzleWorkspaceRepository(db)
+  const useCase = new RegisterUserUseCase(
+    userRepository,
+    workspaceRepository,
+    hashRepository,
+  )
   return useCase
 }
