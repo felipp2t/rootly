d3a05805255bcf0451cbb3f7fe7c0a2d36399fe2
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-06 13:59:11 -0300

    feat: introduce workspace repository and in-memory repo

---
+++ apps/api/src/domain/root/application/repositories/workspace-repository.ts
@@ -0,0 +1,9 @@
+import type { Workspace } from '../../enterprise/entities/workspace.ts'
+
+export abstract class WorkspaceRepository {
+  abstract findById(id: string): Promise<Workspace | null>
+  abstract findByName(name: string): Promise<Workspace | null>
+  abstract findAll(): Promise<Workspace[]>
+  abstract save(Workspace: Workspace): Promise<void>
+  abstract delete(id: string): Promise<void>
+}
---
+++ apps/api/test/repositories/in-memory-workspace-repository.ts
@@ -0,0 +1,33 @@
+import type { WorkspaceRepository } from '@/domain/root/application/repositories/workspace-repository.ts'
+import type { Workspace } from '@/domain/root/enterprise/entities/workspace.ts'
+
+export class InMemoryWorkspaceRepository implements WorkspaceRepository {
+  items: Workspace[] = []
+
+  async findById(id: string): Promise<Workspace | null> {
+    return (
+      this.items.find((Workspace) => Workspace.id.toString() === id) ?? null
+    )
+  }
+
+  async findByName(name: string): Promise<Workspace | null> {
+    return this.items.find((Workspace) => Workspace.name === name) ?? null
+  }
+
+  async findAll(): Promise<Workspace[]> {
+    return this.items
+  }
+
+  async save(Workspace: Workspace): Promise<void> {
+    this.items.push(Workspace)
+  }
+  async delete(id: string): Promise<void> {
+    const WorkspaceIndex = this.items.findIndex(
+      (Workspace) => Workspace.id.toString() === id,
+    )
+
+    if (WorkspaceIndex !== -1) {
+      this.items.splice(WorkspaceIndex, 1)
+    }
+  }
+}
