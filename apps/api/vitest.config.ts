071731d480e7764ac3b84df87cf3cec91acd313d
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-06 18:04:26 -0300

    chore: add e2e test script and Testcontainers deps

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['src/**/*.spec.ts'],
+    exclude: ['src/**/*.e2e.spec.ts'],
     globals: true,
   },
   plugins: [tsconfigPaths()],
