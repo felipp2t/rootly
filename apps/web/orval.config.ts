4a3f6afd0a336cf8a5b45a5500a13a9cc7141fbe
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-07 18:25:34 -0300

    feat: add Orval-generated API client and config

---
+++ apps/web/orval.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'orval'
+
+export default defineConfig({
+  rootly: {
+    input: {
+      target: '../api/openapi.json',
+    },
+    output: {
+      mode: 'tags-split',
+      target: './src/api/index.ts',
+      schemas: './src/api/model',
+      client: 'react-query',
+      baseUrl: 'http://localhost:3000',
+    },
+  },
+})
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { PlusSignIcon } from '@hugeicons/core-free-icons'
+import { HugeiconsIcon } from '@hugeicons/react'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -5,5 +7,13 @@ export const Route = createFileRoute('/')({
 })
 
 function RouteComponent() {
-  return <div className='bg-red-600 size-30'></div>
+  return (
+    <div className='pl-24 pt-24 container mx-auto'>
+      <div className='h-48 w-40 border rounded bg-zinc-950 flex flex-col items-center gap-2.5 justify-center cursor-pointer hover:bg-zinc-900/50 transition-colors px-1'>
+        <HugeiconsIcon icon={PlusSignIcon} size={16} color='#fff' />
+
+        <span className='text-sm flex items-center gap-1'>New Workspace</span>
+      </div>
+    </div>
+  )
 }
