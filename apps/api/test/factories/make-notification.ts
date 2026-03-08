5dad4460d4fe8632e77b6ce95f3a4ad6b1961d61
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-08 17:11:50 -0300

    feat: add notification factory and in-memory repository

---
+++ apps/api/test/factories/make-notification.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker/locale/pt_BR'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id.ts'
+import {
+  Notification,
+  type NotificationProps,
+} from '@/domain/notification/enterprise/entities/notification.ts'
+
+export function makeNotification(
+  override: Partial<NotificationProps> = {},
+  id?: UniqueEntityID,
+) {
+  const notification = Notification.create(
+    {
+      recipientId: new UniqueEntityID(),
+      title: faker.lorem.sentence(4),
+      content: faker.lorem.sentence(10),
+      ...override,
+    },
+    id,
+  )
+
+  return notification
+}
---
+++ apps/api/test/repositories/in-memory-notification-repository.ts
@@ -0,0 +1,28 @@
+import type { NotificationRepository } from '@/domain/notification/application/repositories/notification-repository.ts'
+import type { Notification } from '@/domain/notification/enterprise/entities/notification.ts'
+
+export class InMemoryNotificationRepository implements NotificationRepository {
+  public items: Notification[] = []
+
+  async findById(id: string) {
+    const notification = this.items.find((item) => item.id.toString() === id)
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
+  async create(notification: Notification) {
+    this.items.push(notification)
+  }
+
+  async save(notification: Notification) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === notification.id,
+    )
+
+    this.items[itemIndex] = notification
+  }
+}
