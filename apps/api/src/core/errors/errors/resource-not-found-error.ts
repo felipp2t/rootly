9471c6656b8dbbe92293e31a02956251c172393c
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-09 17:15:42 -0300

    feat: accept optional resource in ResourceNotFoundError

--- apps/api/src/core/errors/errors/resource-not-found-error.ts
+++ apps/api/src/core/errors/errors/resource-not-found-error.ts
@@ -1,7 +1,7 @@
 import type { BaseError } from '../base-error.ts'
 
 export class ResourceNotFoundError extends Error implements BaseError {
-  constructor() {
-    super('Resource not found')
+  constructor(resource?: string) {
+    super(`${resource || 'Resource'} not found`)
   }
 }
