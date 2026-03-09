d99d7cd43bb70d4ce9fa8f0a7049e8c769f2be08
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-09 16:05:00 -0300

    feat: create Owner role and permissions on workspace

--- apps/api/src/domain/root/application/use-cases/create-workspace.ts
+++ apps/api/src/domain/root/application/use-cases/create-workspace.ts
@@ -1,6 +1,13 @@
 import { type Either, right } from '@/core/types/either.ts'
+import {
+  permissionResource,
+  RolePermission,
+} from '../../enterprise/entities/role-permission.ts'
 import { Workspace } from '../../enterprise/entities/workspace.ts'
+import { WorkspaceRole } from '../../enterprise/entities/workspace-role.ts'
+import type { RolePermissionRepository } from '../repositories/role-permission-repository.ts'
 import type { WorkspaceRepository } from '../repositories/workspace-repository.ts'
+import type { WorkspaceRoleRepository } from '../repositories/workspace-role-repository.ts'
 
 interface CreateWorkspaceUseCaseRequest {
   name: string
@@ -10,7 +17,11 @@ interface CreateWorkspaceUseCaseRequest {
 type CreateWorkspaceUseCaseResponse = Either<undefined, { workspaceId: string }>
 
 export class CreateWorkspaceUseCase {
-  constructor(private readonly workspaceRepository: WorkspaceRepository) {}
+  constructor(
+    private readonly workspaceRepository: WorkspaceRepository,
+    private readonly workspaceRoleRepository: WorkspaceRoleRepository,
+    private readonly rolePermissionRepository: RolePermissionRepository,
+  ) {}
 
   async execute({
     name,
@@ -22,6 +33,23 @@ export class CreateWorkspaceUseCase {
     })
 
     await this.workspaceRepository.create(workspace)
+
+    const workspaceRole = WorkspaceRole.create({
+      name: 'Owner',
+      workspaceId: workspace.id.toString(),
+    })
+
+    await this.workspaceRoleRepository.create(workspaceRole)
+
+    for (const resource of permissionResource) {
+      const rolePermission = RolePermission.create({
+        roleId: workspaceRole.id.toString(),
+        resource,
+        action: 'all',
+      })
+
+      await this.rolePermissionRepository.create(rolePermission)
+    }
 
     return right({ workspaceId: workspace.id.toString() })
   }
