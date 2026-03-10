1474fd241c4ccfe78041067db0ee29180da76e9e
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-10 10:38:36 -0300

    feat: allow custom message in NotAllowedError

--- apps/api/src/core/errors/errors/not-allowed-error.ts
+++ apps/api/src/core/errors/errors/not-allowed-error.ts
@@ -1,7 +1,7 @@
 import type { BaseError } from '../base-error.ts'
 
 export class NotAllowedError extends Error implements BaseError {
-  constructor() {
-    super('Not allowed')
+  constructor(message?: string) {
+    super(message ?? 'Not allowed')
   }
 }
