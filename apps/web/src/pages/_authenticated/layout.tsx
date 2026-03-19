ffd8436f12bf8e37076e27469a924e37d57709f3
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-03-19 17:39:47 -0300

    style: lower authenticated layout background opacity

--- apps/web/src/pages/_authenticated/layout.tsx
+++ apps/web/src/pages/_authenticated/layout.tsx
@@ -21,7 +21,7 @@ function RouteComponent() {
   return (
     <div className='relative h-svh'>
       <GridPattern />
-      <div className='relative z-10 flex h-full flex-col bg-zinc-950/90'>
+      <div className='relative z-10 flex h-full flex-col bg-zinc-950/80'>
         <Header />
         <div className='flex flex-1 overflow-hidden'>
           <ScrollArea className='flex-1'>
