d40eb6a17393a5f807ba8656b910e3b9d78a8f07
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-07-12 00:14:21 -0300

    fix(api): remove stray character breaking migration 0014

--- apps/api/drizzle/0014_white_hitman.sql
+++ apps/api/drizzle/0014_white_hitman.sql
@@ -2,4 +2,3 @@ ALTER TABLE "items" DROP CONSTRAINT "items_folder_id_fkey";
 --> statement-breakpoint
 ALTER TABLE "items" ADD COLUMN "archived_at" timestamp with time zone;--> statement-breakpoint
 ALTER TABLE "items" ADD CONSTRAINT "items_folder_id_fkey" FOREIGN KEY ("folder_id") REFERENCES "public"."folders"("id") ON DELETE restrict ON UPDATE no action;
-z 
