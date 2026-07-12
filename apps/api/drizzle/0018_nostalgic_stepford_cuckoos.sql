44851bfb798c9ebf19e9628b813df75766fcd79d
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-07-12 01:30:20 -0300

    chore(api): migrate activity log enums for the member resource

---
+++ apps/api/drizzle/0018_nostalgic_stepford_cuckoos.sql
@@ -0,0 +1,5 @@
+ALTER TYPE "public"."activity_action" ADD VALUE 'member_invited';--> statement-breakpoint
+ALTER TYPE "public"."activity_action" ADD VALUE 'member_joined';--> statement-breakpoint
+ALTER TYPE "public"."activity_action" ADD VALUE 'member_role_changed';--> statement-breakpoint
+ALTER TYPE "public"."activity_action" ADD VALUE 'member_removed';--> statement-breakpoint
+ALTER TYPE "public"."activity_resource_type" ADD VALUE 'member';
